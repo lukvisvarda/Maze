b4255b484498e8157872a6902d5eb0ada6e06401
lukasvisvarda <lvisvarda@gmail.com>
2022-07-01 10:22:59 +0200

    scanner handling

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import logic.FindPathInputReaderFile;
 import logic.FindPathReaderStdIn;
 import java.io.File;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
@@ -10,22 +11,33 @@ public class Main {
 
     private static void read(Scanner sc) throws Exception {
         File maze1 = new File("src/main/resources/maze/maze1.txt");
-        System.out.println("Do you want to read file (f) or standard input (i)? ");
-        String read = sc.nextLine();
+        String read = "";
+
+        while(!Objects.equals(read, "i") && !Objects.equals(read, "f")) {
+            System.out.println("Do you want to read file (f) or standard input (i)? ");
+            read = sc.next();
+        }
         if(read.equals("f")) {
             FindPathInputReaderFile file = new FindPathInputReaderFile();
             file.execute(maze1);
         }
-        else {
-            System.out.println("Please specify how many lines you want to enter: ");
-            int rows = sc.nextInt();
-            String input = "";
+        if(read.equals("i")) {
+            int rows;
+            do {
+                System.out.println("Please specify how many lines you want to enter: ");
+                while (!sc.hasNextInt()) {
+                    System.out.println("That's not a number!");
+                    sc.next();
+                }
+                rows = sc.nextInt();
+            } while (rows <= 0);
+            StringBuilder input = new StringBuilder();
             System.out.println("Please input your maze: ");
             for (int i = 0; i < rows; i++) {
-                input += sc.nextLine() + "\n";
+                input.append(sc.nextLine()).append("\n");
             }
             FindPathReaderStdIn maze = new FindPathReaderStdIn();
-            maze.execute(input);
+            maze.execute(input.toString());
         }
     }
 }
